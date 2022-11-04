09875de1156e0c413b32c0e216cfeed271b9a68b
rdavdin <quochuyd@gmail.com>
2022-11-04 09:06:10 +0700

    add env

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
@@ -6,7 +7,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 })
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=>{
   console.log(`Server listening at port ${port}`);
